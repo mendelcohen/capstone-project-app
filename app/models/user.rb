42038dfac123e92a3dfcf8f4781e56ad5c34e380
Mendel Cohen <mmhakohen@gmail.com>
2020-10-26 21:14:12 -0400

    adds users and sessions routes and implements authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :goals
+
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true 
+  validates :email, presence: true, uniqueness: true 
+  validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
+  end
 end
