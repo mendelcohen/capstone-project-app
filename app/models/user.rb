d50f86cc459e2778ae4c5d32cff8274a36d7e353
Mendel Cohen <mmhakohen@gmail.com>
2021-02-25 13:37:52 -0500

    adds signup validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates :username, presence: true, length: { minimum: 6 }
-  # validates :email, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 5 }
   
 end
